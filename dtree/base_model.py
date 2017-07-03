057fa9c9381d3d572069ec8d41c180003587a905
burningbear <zpcxh95@outlook.com>
2017-07-03 15:43:02 +0800

    use Graph object to manage several compute graph

--- dtree/base_model.py
+++ dtree/base_model.py
@@ -5,15 +5,6 @@
 # Created     :   2017.6.22
 # Modified    :   2017.6.22
 # Version     :   1.0
-
-#!/usr/bin/env python
-# coding=utf-8
-
-# Author      :   Xionghui Chen
-# Created     :   2017.6.22
-# Modified    :   2017.6.22
-# Version     :   1.0
-
 import tensorflow as tf
 # from dtree import Tree, Data, USE_NEAREST,unique,Forest
 import numpy as np
@@ -21,33 +12,46 @@ import random
 import csv
 import logging
 from base_data_structure import *
+import time
 logging.basicConfig(level=logging.INFO)
 
 class Basic_model(object):
 
     def __init__(self,freatures_amount,class_amount,global_flag, 
-        learning_rate=0.01,min_learning_rate=0.00001,tol=0.0001,max_iter=4,
-        multi_layer=None,hidden_layer=30,
-        batch_size=500,
+        learning_rate=0.01,min_learning_rate=0.00001,tol=0.00001,max_iter=40,
+        multi_layer=None,hidden_layer=150,
+        # batch_size=300
         ):
 
         self.freatures_amount = freatures_amount
         self.class_amount = class_amount
         self.learning_rate = learning_rate
         self.min_learning_rate = min_learning_rate
-        self.batch_size = batch_size
-        self.global_flag = global_flag
+        # self.batch_size = batch_size
+        self.model_name = global_flag+'-'+str(self.freatures_amount) + '-' + str(self.class_amount)
         self.tol=tol
         self.max_iter = max_iter
         self.weight = {}
         self.bias = {}
-        self.x = tf.placeholder('float',[None,self.freatures_amount])
-        self.y = tf.placeholder('float', [None, self.class_amount])
-        self.pred = self.default_multi_layer(hidden_layer) if multi_layer == None else multi_layer
-        self.cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(self.pred, self.y))
-        self.saver = tf.train.Saver()
+        self.g = tf.Graph()
+        with self.g.as_default():
+            self.x = tf.placeholder('float',[None,self.freatures_amount])
+            self.y = tf.placeholder('float', [None, self.class_amount])
+            self.Y = tf.placeholder('float',[None,self.class_amount])
+            self.pred = self.default_multi_layer(hidden_layer) if multi_layer == None else multi_layer
+            self.cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(self.pred, self.y))
+            self.optimizer = tf.train.AdamOptimizer(self.learning_rate).minimize(self.cost)
+            self.init = tf.initialize_all_variables()
+            self.predict_single = tf.argmax(self.pred, 1)
+            correct_prediction = tf.equal(tf.argmax(self.pred, 1), tf.argmax(self.y,1))
+            self.accuracy = tf.reduce_mean(tf.cast(correct_prediction,'float'))
+            # self.saver = tf.train.Saver()
+            self.g.finalize()
+        self.sess = tf.Session(graph=self.g)
         self.incremental_times = 0
         self.last_weight = []
+        # 可以在一个类里面初始化一个会话的示例，供其他方法调用，而不是每一个方法开启一个会话然后再关闭
+
 
     def _onehot(self,labels):
         ''' one-hot 编码 '''
@@ -89,77 +93,85 @@ class Basic_model(object):
             Y: label data, numpy type
 
         """
-        learning_rate = self.learning_rate
-        with tf.Session() as sess:
-            optimizer = tf.train.AdamOptimizer(learning_rate).minimize(self.cost)
-            if initial:
-                # 若要进行增量训练，则将initial 设置为false。只有第一次训练的时候需要进行参数的初始化
-                # 初始化需要在计算图构造完成后进行，不能在初始化完成后进行新增的计算图
-                init = tf.initialize_all_variables()
-                sess.run(init)
-                print ("complete initial")
-            else:
-                # 先进行初始化，然后再载入模型！
-                sess.run(tf.initialize_all_variables())
-                ckp = self.saver.last_checkpoints[-1]
-                logging.info("load model, checkpoints %s"%ckp)
-                # self.saver = tf.train.import_meta_graph(''.join([ckp,'.meta']))
-                logging.info("load model, checkpoints %s"%self.saver.last_checkpoints)
-                self.saver.restore(sess, ckp)
-                logging.info("restore finfish")
-                self.last_weight = sess.run(self.weight['h1'])
+        # with self.g.as_default():
+        # with tf.Session() as self.sess:
 
-            n_sample = X.shape[0]
-            print ("xshape %s"%n_sample)
-            Y = self._onehot(Y)
-            print ("yshape %s"%Y.shape[0])
-            last_cost = 999999999999999999999999999
+        if initial:
+            # 若要进行增量训练，则将initial 设置为false。只有第一次训练的时候需要进行参数的初始化
+            # 初始化需要在计算图构造完成后进行，不能在初始化完成后进行新增的计算图
+            
+            self.sess.run(self.init)
+            print ("complete initial")
+        else:
+            # 先进行初始化，然后再载入模型！
+            # self.sess.run(tf.initialize_all_variables())
+            # ckp = self.saver.last_checkpoints[-1]
+            # # logging.info("load model, checkpoints %s"%ckp)
+            # # self.saver = tf.train.import_meta_graph(''.join([ckp,'.meta']))
+            # # logging.info("load model, checkpoints %s"%self.saver.last_checkpoints)
+            # self.saver.restore(self.sess, ckp)
+            # # logging.info("restore finfish")
+            # self.last_weight = self.sess.run(self.weight['h1'])
+            pass
 
-            for epoch in range(self.max_iter):
-                # 分批次进行迭代训练
-                avg_cost = 0.
-                total_batch = int(n_sample / self.batch_size)
-                for i in range(total_batch):
-                    # note:  dot sumbol usage??
-                    _, c = sess.run([optimizer,self.cost],
-                        feed_dict={self.x:X[i*self.batch_size : (i+1)*self.batch_size , :],
-                                   self.y:Y[i*self.batch_size : (i+1)*self.batch_size , :]})
-                    avg_cost += c/total_batch
-                
+        n_sample = X.shape[0]
+        Y = self._onehot(Y)
+        last_cost = 999999999999999999999999999
+        # total_batch = int(n_sample / self.batch_size)
+        # logging.info("total batch is %s"%total_batch)
+        for epoch in range(self.max_iter):
+            _, current_cost = self.sess.run([self.optimizer,self.cost],
+                feed_dict={self.x:X,
+                           self.y:Y})
+            # 分批次进行迭代训练
+            
+            # avg_cost = 0.
+
+            # for i in range(total_batch):
+            #     # note:  dot sumbol usage??
+            #     _, c = self.sess.run([self.optimizer,self.cost],
+            #         feed_dict={self.x:X[i*self.batch_size : (i+1)*self.batch_size , :],
+            #                    self.y:Y[i*self.batch_size : (i+1)*self.batch_size , :]})
+            #     avg_cost += c/total_batch
+            
+            if display_step:
+                logging.info("Epoch: %s, Cost = %s"%(epoch+1,current_cost))
+
+            if(last_cost - current_cost < self.tol):
                 if display_step:
-                    logging.info("Epoch: %s, Cost = %s"%(epoch+1,avg_cost))
-
-                if(last_cost - avg_cost < self.tol):
-                    # adaptive learning rate
-                    learning_rate = learning_rate / 5
-                    logging.info("cost decrease less than %s, decrease learning rate to %s"%(self.tol,learning_rate))
-                    if self.min_learning_rate > learning_rate:
-                        logging.info("learning rate is small than %s, break epoch"%self.min_learning_rate)
-                        break
-                last_cost = avg_cost
-            logging.info('Opitimization Finished!')
-            this_weight = sess.run(self.weight['h1'])
-            if self.last_weight !=[]:
-                a = tf.equal(self.last_weight, this_weight)
-                print sess.run(a)
-            print self.saver.save(sess, self.global_flag)
-            print self.saver.last_checkpoints
-            self.incremental_times+=1
+                    logging.info("cost decrease less than %s,break"%(self.tol))
+                break
+                # adaptive learning rate
+                # learning_rate = learning_rate / 5
+                # if self.min_learning_rate > learning_rate:
+                #     # logging.info("learning rate is small than %s, break epoch"%self.min_learning_rate)
+                #     break
+            last_cost = current_cost
+        logging.info('Opitimization Finished!')
+        this_weight = self.sess.run(self.weight['h1'])
+        # if self.last_weight !=[]:
+        #     a = tf.equal(self.last_weight, this_weight)
+        #     print self.sess.run(a)
+        # print self.saver.save(self.sess, self.model_name)
+        # print self.saver.last_checkpoints
+        self.incremental_times+=1
 
     def predict(self,X):
-        with tf.Session() as sess:
-            self.saver.restore(sess, self.saver.last_checkpoints[0])
-            predict = sess.run(tf.argmax(self.pred, 1),feed_dict={self.x:X})
-            return predict
+        # with self.g.as_default():
+        #     with tf.Session() as self.sess:
+                # with tf.Session() as sess:
+        # self.saver.restore(self.sess, self.saver.last_checkpoints[0])
+        predict = self.sess.run(self.predict_single,feed_dict={self.x:X})
+        return predict
 
     def test(self,X,Y):
-        with tf.Session() as sess:
-            Y = self._onehot(Y)
-            self.saver.restore(sess, self.saver.last_checkpoints[0])  
-            correct_prediction = tf.equal(tf.argmax(self.pred, 1), tf.argmax(Y,1))
-            accuracy = tf.reduce_mean(tf.cast(correct_prediction,'float'))
-            acc = sess.run(accuracy,feed_dict={self.x:X,self.y:Y})
-            return acc
+        # with tf.Session() as self.sess:
+        # with self.g.as_default():
+        #     with tf.Session() as self.sess:
+        Y = self._onehot(Y)
+        # self.saver.restore(self.sess, self.saver.last_checkpoints[0])  
+        acc = self.sess.run(self.accuracy,feed_dict={self.x:X,self.y:Y})
+        return acc
 
 
 def main():
@@ -172,7 +184,7 @@ def main():
     res_label = []
     size = len(ori)
     incre_times = 10
-    batch_size = 200
+    batch_size = 4000
     for list_item in ori:
         sample_item = []
         for key,value in list_item.items():
@@ -184,13 +196,6 @@ def main():
     res_data = np.array(res_data)
     res_label = np.array(res_label)
 
-    freatures_amount = res_data.shape[1]
-    model = Basic_model(freatures_amount, 2 , global_flag='base_model_para')
-
-    model.train(res_data[:batch_size], res_label[:batch_size],initial=True,display_step=True)
-    print res_data[0].reshape(1,14)
-    print model.predict(res_data[0].reshape(1,14)) 
-    print (" true value is %s"%res_label[0])
 
     test = Data('dataset/uci_adult/delete_adult2.test')
     ori = list(test)
@@ -207,13 +212,37 @@ def main():
         res_data_test.append(sample_item)
     res_data_test = np.array(res_data_test)
     res_label_test = np.array(res_label_test)
-    print model.test(res_data_test,res_label_test)
 
+    freatures_amount = res_data.shape[1]
+    model_list = []
+    for i in range(10):
+        start = time.clock()
+        logging.info("initial model %s"%i)
+        model_list.append(Basic_model(freatures_amount, 2 , global_flag='base_model_para'))
+        end = time.clock()
+        print "time used %s"%(end-start)
 
-    for i in range(5):
-        print "incremental learning times %s"%i
-        model.train(res_data[batch_size*(i+1):batch_size*(i+2)], res_label[batch_size*(i+1):batch_size*(i+2)],initial=False,display_step=True)
+    for model in model_list:
+        start = time.clock()
+        model.train(res_data[:batch_size], res_label[:batch_size],initial=True,display_step=True)
+        end = time.clock()
+        print "time used of training: %s"%(end-start)
+        pre =  model.predict(res_data[0].reshape(1,14)) 
+        start = time.clock()
+        end = time.clock()
+        print ("predict is %s true value is %s"%(pre,res_label[0]))
+        print "time used of predict: %s"%(end-start)
+        start = time.clock()       
         print model.test(res_data_test,res_label_test)
+        end = time.clock()
+        print "time used of test: %s"%(end-start)        
+        start = time.clock()             
+        for i in range(5):
+            print "incremental learning times %s"%i
+            model.train(res_data[batch_size*(i+1):batch_size*(i+2)], res_label[batch_size*(i+1):batch_size*(i+2)],initial=False,display_step=True)
+            print model.test(res_data_test,res_label_test)
+        end = time.clock()
+        print "time used of incremental learning: %s"%(end-start)      
 
 if __name__ == '__main__':
     main()
